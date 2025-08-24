580d091db0cf428cb61d1e5e5db52f3aea6f4a64
Konrad Klawonn <k.klawonn@mailbox.org>
2025-08-24 14:59:39 +0200

    Make Tabs work

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
   import { Events } from "@wailsio/runtime";
 
-  let tableNames: string[] = [];
+  let tableNames: string[] = ["tab 1", "tab 2"];
 
   Events.On("dbLoaded", (tn: any) => {
     tableNames = tn.data[0];
     console.log(tableNames);
-    console.log(tn.data[0]);
+    console.log(tn.data);
   });
 </script>
 
-<h1>SQLite Viewer</h1>
-{#if tableNames}
-  {#each tableNames as db}
-    <button class="btn">{db}</button>
-  {/each}
-{/if}
+<h1 class="text-2xl">SQLite Viewer</h1>
+<div class="tabs tabs-lift tabs-xl">
+  {#if tableNames}
+    {#each tableNames as db}
+      <input type="radio" class="tab" name="db_selected" aria-label={db} />
+      <div class="tab-content bg-base-100 border-base-300 p-6">{db}</div>
+    {/each}
+  {/if}
+</div>
